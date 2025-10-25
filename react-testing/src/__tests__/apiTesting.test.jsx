a996e9d0ec5691cf4f4c34225eebf37d59ca38ab
codeher-rahavi <rahaviganeshan529@gmail.com>
2025-10-25 16:06:11 +0530

    commit

--- react-testing/src/__tests__/apiTesting.test.jsx
+++ react-testing/src/__tests__/apiTesting.test.jsx
@@ -1,18 +1,24 @@
+import { render } from "@testing-library/react";
+import ApiCallTest from "../components/api-testing";
 
-
-
-describe('Api testing file',()=>{
-    it('check list of users fetching',()=>{
+describe('Api testing file', () => {
+    it('check list of users fetching', () => {
         global.fetch = jest.fn(() => Promise.resolve({
-            json : () => Promise.resolve([
+            json: () => Promise.resolve([
                 {
-                    id:1,
-                    name:"rahavi",
-                    id:2,
-                    name : "hardik",
+                    id: 1,
+                    name: "rahavi",
+                    id: 2,
+                    name: "hardik",
 
-                }
-            ])
-        }))
-    })
-})
+                },
+            ]),
+        })
+        );
+
+
+        render(<ApiCallTest />)
+
+        expect(screen.getByText('Loading data.....').toBe)
+    });
+});
